c8d9b1027884e59a6a00370dfa530a308f8a88b6
Retro <nicky.zomaar@gmail.com>
2022-08-19 13:42:57 +0200

    Initial commit

---
+++ src/main/java/net/retrotrack/hardcored/HardCored.java
@@ -0,0 +1,15 @@
+package net.retrotrack.hardcored;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HardCored implements ModInitializer {
+	public static final String MOD_ID = "hardcored";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/retrotrack/hardcored/HardCoredClient.java
@@ -0,0 +1,10 @@
+package net.retrotrack.hardcored;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HardCoredClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
