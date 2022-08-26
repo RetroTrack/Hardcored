9e6ed3a38f870e96fa86219480c69eb099763813
Retro <nicky.zomaar@gmail.com>
2022-08-26 10:15:33 +0200

    yeah did a lot of things, but for some reason all server side stuff depends on one thing, and yeah that doesn't work DeathMixin.java is the thing that doesnt btw

--- src/main/java/net/retrotrack/hardcored/mixin/DeathMixin.java
+++ src/main/java/net/retrotrack/hardcored/mixin/DeathMixin.java
@@ -1,26 +1,18 @@
 package net.retrotrack.hardcored.mixin;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.retrotrack.hardcored.HardCored;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.UUID;
-
 @Mixin(ServerPlayerEntity.class)
 public abstract class DeathMixin {
-    @Shadow public abstract Entity getCameraEntity();
-
     @Inject(at = @At("HEAD"), method = "onDeath")
-    public void asdf(DamageSource source, CallbackInfo ci){
-        String uuid = this.getCameraEntity().getUuidAsString();
-        ServerPlayerEntity player = HardCored.serverReference.getValue().getPlayerManager().getPlayer(UUID.fromString(uuid));
+    public void onDeath(DamageSource source, CallbackInfo ci){
+        ServerPlayerEntity player = (ServerPlayerEntity)(Object)this;
         player.sendMessage(Text.literal("player died"));
     }
 
